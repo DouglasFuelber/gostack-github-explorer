1682464c31144761908e1a7e5bf247107c17d5df
Douglas Fuelber <contato@douglasfuelber.com>
2020-04-22 00:09:18 -0300

    Get repos from Github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,73 +1,67 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Gitub Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a
-          href="https://github.com/DouglasFuelber/douglas-fuelber-gatsby"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://avatars0.githubusercontent.com/u/15067098?s=460&u=937ff779f5b702ee7cfc32da353fa97de9ecf252&v=4"
-            alt="DouglasFuelber"
-          />
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="https://github.com/">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>douglas-fuelber-gatsby</strong>
-            <p>Personal website build with Gatsby and hosted on Netlify.</p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a
-          href="https://github.com/DouglasFuelber/douglas-fuelber-gatsby"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://avatars0.githubusercontent.com/u/15067098?s=460&u=937ff779f5b702ee7cfc32da353fa97de9ecf252&v=4"
-            alt="DouglasFuelber"
-          />
-
-          <div>
-            <strong>douglas-fuelber-gatsby</strong>
-            <p>Personal website build with Gatsby and hosted on Netlify.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a
-          href="https://github.com/DouglasFuelber/douglas-fuelber-gatsby"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://avatars0.githubusercontent.com/u/15067098?s=460&u=937ff779f5b702ee7cfc32da353fa97de9ecf252&v=4"
-            alt="DouglasFuelber"
-          />
-
-          <div>
-            <strong>douglas-fuelber-gatsby</strong>
-            <p>Personal website build with Gatsby and hosted on Netlify.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
